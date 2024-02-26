7590f2ba8586ff590c94cd28ca982379487d7301
Kipitril <hezkipkorir@gmail.com>
2024-02-26 21:56:23 +0300

    project 1

---
+++ models/base_model.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Contains class BaseModel
+"""
+
+from datetime import datetime
+import models
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+import uuid
+
+time = "%Y-%m-%dT%H:%M:%S.%f"
+
+if models.storage_t == "db":
+    Base = declarative_base()
+else:
+    Base = object
